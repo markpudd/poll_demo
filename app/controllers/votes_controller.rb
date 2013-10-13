d8ba68b0dec1526717b250c769eecc245d69d972
Mark Puddick <mark@moroku>
2013-10-14 06:44:49 +1100

    Fixed votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,8 +4,9 @@ class VotesController < ApplicationController
   # POST /votes
   # POST /votes.json
   def create
-    @vote = Vote.new(params[:vote])
-
+    @vote = Vote.new
+    
+    @vote.answer_id = params[:answer_id]
     respond_to do |format|
       if @vote.save
         format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ActiveRecord::Base
   belongs_to :answer
   belongs_to :player
-  # attr_accessible :title, :body
+   attr_accessible :answer_id
 end
